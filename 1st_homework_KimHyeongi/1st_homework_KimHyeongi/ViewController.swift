ed251fbdb7eb5e9a80f9462572280226458c70f0
Hyeongi Kim <21900215@handong.ac.kr>
2024-03-26 00:14:46 +0900

    [Add] 1차 세미나 과제

---
+++ 1st_homework_KimHyeongi/1st_homework_KimHyeongi/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  1st_homework_KimHyeongi
+//
+//  Created by 김현기 on 3/23/24.
+//
+
+import Foundation
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
